0a38f870b6e86d3bd75f5467b13924d04697b072
DCabahug1 <150985638+DCabahug1@users.noreply.github.com>
2025-09-25 19:34:30 -0700

    Update Contact.tsx

--- components/sections/contact/Contact.tsx
+++ components/sections/contact/Contact.tsx
@@ -9,14 +9,14 @@ function Contact() {
       className="flex items-center justify-center h-min p-8 w-full"
     >
       <div className="  flex gap-16 h-min w-min">
-        <a href="mailto:duanecabahug6@gmail.com" target="_blank">
-          <i className="fa-solid fa-envelope text-6xl hover:text-foreground/60 transition-all duration-200"></i>
+        <a href="mailto:duanecabahug6@gmail.com" target="_blank" className="">
+          <i className="fa-solid fa-envelope text-6xl hover:text-primary transition-all duration-200"></i>
         </a>
         <a href="https://github.com/dcabahug1" target="_blank">
-          <i className="fa-brands fa-github text-6xl hover:text-foreground/60 transition-all duration-200"></i>
+          <i className="fa-brands fa-github text-6xl hover:text-primary transition-all duration-200"></i>
         </a>
         <a href="https://linkedin.com/in/duane-cabahug" target="_blank">
-          <i className="fa-brands fa-linkedin text-6xl hover:text-foreground/60 transition-all duration-200"></i>
+          <i className="fa-brands fa-linkedin text-6xl hover:text-primary transition-all duration-200"></i>
         </a>
       </div>
     </div>
